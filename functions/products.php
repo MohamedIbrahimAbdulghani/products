30b6aa2a47b89c874660b1333fe14151db7c44a9
Mohamed Ibrahim <mohamedibrahimabdulghani@gmail.com>
2023-09-07 16:31:25 +0300

    get projects from database and show in project

--- functions/products.php
+++ functions/products.php
@@ -2,8 +2,21 @@
 
 require_once "db.php";
 
+// this is function to add project to database
 function addProjects($product_name, $product_price, $product_image) {
     global $connection_database;
     $sql = "INSERT INTO `product` (`product_name`, `product_price`, `product_image`) VALUES ('$product_name', '$product_price', '$product_image') ";
     mysqli_query($connection_database, $sql);
+}
+
+// this is function to get projects from database
+function getProjects() {
+    global $connection_database;
+    $sql = "SELECT * FROM `product` ";
+    $result = mysqli_query($connection_database, $sql);
+    $projects = [];
+    while($res = mysqli_fetch_assoc($result)):
+        $projects[] = $res;
+    endwhile;
+    return $projects;
 }
---
+++ products.php
@@ -0,0 +1,52 @@
+<?php
+    require_once "functions/products.php";
+
+
+    $result = getProjects();
+
+    if(empty($result)) {
+        echo "<h2>جميع المنتجات المتوفرة</h2>";
+        echo "<h2>Don't Found Any Project</h2>";
+        header("Refresh:1;url=index.php");
+    }
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Products | المنتجات</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@200&family=Roboto:ital,wght@1,100&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    
+        <center>
+        <?php if(!empty($result)): ?>
+            <h2>جميع المنتجات المتوفرة</h2>
+            <?php foreach($result as $res): ?>
+                <div class="card" style="width: 18rem;" >
+                    <img class="card-img-top" src="<?php echo "images/" .$res["product_image"] ?>" alt="Card image cap" class="img-fluid">
+                    <div class="card-body">
+                        <h5 class="card-title">Name Is : <?php echo $res['product_name'] ?></h5>
+                        <p class="card-text">Price Is : <?php echo $res["product_price"] ?></p>
+                        <a href="#" class="btn btn-danger">حذف منتج</a>
+                        <a href="#" class="btn btn-primary">تعديل منتج</a>
+                    </div>
+                </div>
+            <?php endforeach; ?>
+            
+        <?php endif; ?>
+
+
+        </center>
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+</body>
+</html>
